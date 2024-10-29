209f76a40903caab5e78d9e53b85f4dc7f4e51c1
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-10-29 17:21:18 +0500

    Task0

--- Tyuiu.ShakhovDK.Sprint3.Task0.V12.Lib/DataService.cs
+++ Tyuiu.ShakhovDK.Sprint3.Task0.V12.Lib/DataService.cs
@@ -6,10 +6,10 @@ namespace Tyuiu.ShakhovDK.Sprint3.Task0.V12.Lib
         public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
             int i;
-            double p = 1.5;
-            for (i = (startValue + 1); i <= stopValue; i++)
+            double p = 1;
+            for (i = (startValue); i <= stopValue; i++)
             {
-                p *= Math.Pow(value, i) + (1 / (i + 1));
+                p *= Math.Pow(value, i) + (1.0 / (i + 1));
             }
             return Math.Round(p, 3);
         }
