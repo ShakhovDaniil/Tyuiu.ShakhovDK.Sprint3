c18c0116c3ce0e8c96f96f423f056b2bc7316ca0
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-10-31 21:58:33 +0500

    Task2

---
+++ Tyuiu.ShakhovDK.Sprint3.Task2.V8.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.ShakhovDK.Sprint3.Task2.V8.Lib
+{
+    public class DataService : ISprint3Task2V8
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double p = 1;
+            do
+            {
+                p *= (1.0 / Math.Pow(Math.Cos(5) + 1.0, 2));
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(p, 3);
+        }
+    }
+}
