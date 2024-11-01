0cbf6782076a373b89a2e686a32af9b3b080ebfb
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-11-02 02:03:08 +0500

    Task5

--- Tyuiu.ShakhovDK.Sprint3.Task5.V18.Lib/DataService.cs
+++ Tyuiu.ShakhovDK.Sprint3.Task5.V18.Lib/DataService.cs
@@ -8,9 +8,9 @@ namespace Tyuiu.ShakhovDK.Sprint3.Task5.V18.Lib
             double res = 0;
             for (int i = startValue1; i <= stopValue1; i++)
             {
-                for (int j = startValue2; j <= stopValue2; j++)
+                for (int k = startValue2; k <= stopValue2; k++)
                 {
-                    res += Math.Cos(x) + (Math.Pow(startValue2, 2) / 2.0);
+                    res += Math.Cos(x) + (Math.Pow(k, 2) / 2);
                 }
             }
             return Math.Round(res, 3);
--- Tyuiu.ShakhovDK.Sprint3.Task5.V18.Test/DataServiceTest.cs
+++ Tyuiu.ShakhovDK.Sprint3.Task5.V18.Test/DataServiceTest.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.ShakhovDK.Sprint3.Task5.V18.Test
             int startValue2 = 1;
             int stopValue2 = 11;
             double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
-            double wait = 25.861;
+            double wait = 768.361;
             Assert.AreEqual(wait, res);
         }
     }
