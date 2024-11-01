0358593770178a881b8886760dacf32f46000f10
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-11-02 01:57:34 +0500

    Task5

--- Tyuiu.ShakhovDK.Sprint3.Task5.V18.Lib/DataService.cs
+++ Tyuiu.ShakhovDK.Sprint3.Task5.V18.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.ShakhovDK.Sprint3.Task5.V18.Lib
 {
-    public class DataService : ISprint3Task5V20
+    public class DataService : ISprint3Task5V18
     {
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
