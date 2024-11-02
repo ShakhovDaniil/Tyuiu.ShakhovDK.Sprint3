c54abbe6ea540e150364e6307bb127ae2b34d75d
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-11-02 07:17:55 +0500

    Task7

--- Tyuiu.ShakhovDK.Sprint3.Task7.V8.Lib/DataService.cs
+++ Tyuiu.ShakhovDK.Sprint3.Task7.V8.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ShakhovDK.Sprint3.Task7.V8.Lib
         public double[] GetMassFunction(int startValue, int stopValue)
         {
             double[] valueArray;
-            int len = stopValue - startValue + 1;
+            int len = (stopValue - startValue) + 1;
             valueArray = new double[len];
             double y;
             int index = 0;
@@ -18,7 +18,7 @@ namespace Tyuiu.ShakhovDK.Sprint3.Task7.V8.Lib
                 }
                 else
                 {
-                    y = Math.Round(Math.Sin(x) + (Math.Cos(x) + 1 / (2 - x)) + 2 * x, 2);
+                    y = Math.Round((Math.Sin(x) + ((Math.Cos(x) + 1.0) / (2 - x)) + 2.0 * x), 2);
                     valueArray[index] = y;
                 }
                 index++;
